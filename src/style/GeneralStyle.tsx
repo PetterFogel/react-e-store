c7b784da63e0a44dd8ceef32ebccb7527a07cf33
Oliver Johansson <oliverjohansson3@gmail.com>
2021-03-09 15:33:57 +0100

    Add stylingtsx

---
+++ src/style/GeneralStyle.tsx
@@ -0,0 +1,44 @@
+import { CSSProperties } from "@material-ui/styles";
+
+export const btnLarge: CSSProperties = {
+    padding: '1.5rem 2.5rem',
+    borderRadius: '3rem',
+    outline: 'none',
+    fontSize: '1.7rem',
+    background: '#56EAC6',
+    color: '#fff',
+    fontWeight: 'bold',
+    border: 'none',
+    cursor: 'pointer',
+
+}
+
+export const btnMedium: CSSProperties = {
+    padding: '1.1rem 2rem',
+    borderRadius: '2.5rem',
+    outline: 'none',
+    fontSize: '1.3rem',
+    background: '#56EAC6',
+    color: '#fff',
+    fontWeight: 'bold',
+    border: 'none',
+    cursor: 'pointer'
+}
+
+export const btnSmall: CSSProperties = {
+    padding: '1.5rem 2.5rem',
+    borderRadius: '3rem',
+    outline: 'none',
+    fontSize: '1.7rem',
+    background: '#56EAC6',
+    color: '#fff',
+    fontWeight: 'bold',
+    border: 'none',
+    cursor: 'pointer'
+}
+
+
+
+
+
+
