659e7d3627471e1b32b702462fc3c1417ae41620
Oliver Johansson <oliverjohansson3@gmail.com>
2021-03-10 15:07:52 +0100

    Add context logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/Navbar.css'
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import { CartContext } from '../contexts/CartContext'
 
-function Navbar() {
-    return (
-      <header className="main-header">
-        <Link style={{ textDecoration: "none" }} to="/">
-          <h2 className="header-title">ShoeWay</h2>
-        </Link>
-        <nav>
-          <ul className="nav-links">
-            <Link style={{textDecoration: 'none', color: '#000'}} to="/products">
-              <li>Products</li>
-            </Link>
-            <li>
-              <ShoppingCartOutlinedIcon/>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
+class Navbar extends Component {
+    context!: React.ContextType<typeof CartContext>
+    static contextType = CartContext;
+    
+    render() {    
+      return (
+        <header className="main-header">
+          <Link style={{ textDecoration: "none" }} to="/">
+            <h2 className="header-title">ShoeWay</h2>
+          </Link>
+          <nav>
+            <ul className="nav-links">
+              <Link style={{textDecoration: 'none', color: '#000'}} to="/products">
+                <li>Products</li>
+              </Link>
+              <li>
+                <ShoppingCartOutlinedIcon/>
+              </li>
+              <li>
+                {this.context.cart.length}
+              </li>
+            </ul>
+          </nav>
+        </header>
+      );
+    }
 }
 
 export default Navbar
