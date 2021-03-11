80ab5f92e6f49f64169c075194300288dabac2a9
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-11 09:55:55 +0100

    changed the styling

--- src/components/Checkout/PaymentMethods/BankTransfer.tsx
+++ src/components/Checkout/PaymentMethods/BankTransfer.tsx
@@ -1,11 +1,47 @@
+import { Button, TextField } from '@material-ui/core'
+import { CSSProperties } from '@material-ui/styles';
 import React from 'react'
 
 const BankTransfer = () => {
     return (
-        <div>
-            
-        </div>
-    )
+      <div className="modal-container">
+        <form className="form">
+          <h2 style={{ textAlign: "center" }}>Bank Transfer</h2>
+          <TextField
+            margin="normal"
+            label="Bank"
+            name="bankname"
+            variant="outlined"
+            required
+            InputLabelProps={{ shrink: true }}
+          />
+          <TextField
+            margin="normal"
+            label="Account Number"
+            name="accountNumber"
+            variant="outlined"
+            required
+            InputLabelProps={{ shrink: true }}
+          />
+          <Button variant="contained" type="submit" style={btn}>
+            Complete Payment
+          </Button>
+        </form>
+      </div>
+    );
 }
 
 export default BankTransfer
+const btn: CSSProperties = {
+  alignSelf: "center",
+  bordeeRadius: "3rem",
+  outline: "none",
+  fontSize: "1rem",
+  background: "#56EAC6",
+  color: "#fff",
+  fontWeight: "bold",
+  border: "none",
+  cursor: "pointer",
+  margin: "1rem",
+  padding: "1rem",
+};
--- src/components/Checkout/PaymentMethods/PaymentMethod.tsx
+++ src/components/Checkout/PaymentMethods/PaymentMethod.tsx
@@ -1,13 +1,50 @@
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@material-ui/core'
+import { CSSProperties } from '@material-ui/styles';
 import React from 'react'
 import '../../../style/Form.css'
 
 const PaymentMethod = () => {
-    return <div className="modal-container">
-        <h3 style={{fontSize: '1.4rem', margin: '1rem'}}>Choose payment method</h3>
-        <div className="container">Credit card</div>
-        <div className="container">Swish</div>
-        <div className="container">Bank Transfer</div>
-    </div>;
+    return (
+      <div className="modal-container">
+        <h3 style={{ fontSize: "1.2rem", margin: "1rem" }}>
+          Choose payment method
+        </h3>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Payment Method</FormLabel>
+          <RadioGroup aria-label="payment">
+            <FormControlLabel
+              value="Credit Card"
+              control={<Radio />}
+              label="Credit Card"
+            />
+            <FormControlLabel value="Swish" control={<Radio />} label="Swish" />
+            <FormControlLabel
+              value="Bank Transfer"
+              control={<Radio />}
+              label="Bank Transfer"
+            />
+          </RadioGroup>
+        </FormControl>
+        <Button variant="contained" type="submit" style={btn}>
+          Next
+        </Button>
+      </div>
+    );
 }
 
 export default PaymentMethod
+
+
+const btn: CSSProperties = {
+  alignSelf: "center",
+  bordeeRadius: "3rem",
+  outline: "none",
+  fontSize: "1rem",
+  background: "#56EAC6",
+  color: "#fff",
+  fontWeight: "bold",
+  border: "none",
+  cursor: "pointer",
+  margin: "1rem",
+  padding: "1rem",
+};
