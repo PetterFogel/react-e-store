11ae4d7cccc47aa81199b5785b68eb491739ad1d
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-16 15:50:08 +0100

    added stepper

--- src/components/Checkout/Accordian/Accordian.tsx
+++ src/components/Checkout/Accordian/Accordian.tsx
@@ -2,14 +2,52 @@ import React from 'react'
 import PersonalDetails from './PersonalDetails';
 import DelivaryDetails from './DelivaryDetails';
 import PaymentDetails from './PaymentDetails';
+import { Button, Step, StepLabel, Stepper } from '@material-ui/core';
+import { Label } from '@material-ui/icons';
+import { CSSProperties } from '@material-ui/styles';
 
 
 const Accordian = () => {
+
+  const [activeStep, setActiveStep] = React.useState(0)
+
+  const nextStep = () => {
+    if(activeStep < 2)
+        setActiveStep((currentStep) => currentStep + 1)
+  }
+
+  const previousStep = () => {
+    if(activeStep !== -1)
+      setActiveStep((currentStep) => currentStep - 1)
+  }
+
     return (
       <div>
-        <PersonalDetails/>
-        {/* <DelivaryDetails/>
-        <PaymentDetails/> */}
+        <Stepper activeStep={activeStep} orientation="vertical">
+          <Step>
+            <StepLabel>
+              <PersonalDetails />
+            </StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>
+              <DelivaryDetails />
+            </StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>
+              <PaymentDetails />
+            </StepLabel>
+          </Step>
+        </Stepper>
+        
+          <Button onClick={() => nextStep()} variant="outlined">
+            Next Step
+          </Button>
+          <Button onClick={() => previousStep()} variant="outlined">
+            Previous Step
+          </Button>
+
       </div>
     );
 }
