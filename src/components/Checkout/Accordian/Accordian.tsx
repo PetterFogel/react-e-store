70dfd03c277d7d748b96b9888c261b369d52a208
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-17 10:15:33 +0100

    mapping each form

--- src/components/Checkout/Accordian/Accordian.tsx
+++ src/components/Checkout/Accordian/Accordian.tsx
@@ -4,10 +4,31 @@ import PaymentDetails from './PaymentDetails';
 import { Button, Step, StepLabel, Stepper } from '@material-ui/core';
 import { Label } from '@material-ui/icons';
 import { CSSProperties } from '@material-ui/styles';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
+
+
+  function getSteps(){
+    return [ <PersonalDetails/>, <DelivaryDetails/>, <PaymentDetails/>];
+  }
+
+  function getStepContent(stepIndex: number){
+    switch (stepIndex) {
+      case 0: 
+        return <PersonalDetails/>;
+      case 1: 
+        return <DelivaryDetails/>;
+      case 2: 
+        return <PaymentDetails/>;
+      default:
+        return 'unknown step'; 
+    }
+  }
 const Accordian = () => {
 
   const [activeStep, setActiveStep] = React.useState(0)
+  const steps = getSteps();
 
   const nextStep = () => {
     if(activeStep < 2)
@@ -21,31 +42,32 @@ const Accordian = () => {
 
     return (
       <div>
-        <Stepper activeStep={activeStep} orientation="vertical">
-          <Step>
-            <StepLabel>
-              <PersonalDetails />
-            </StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>
-              <DelivaryDetails />
-            </StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>
-              <PaymentDetails />
-            </StepLabel>
-          </Step>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label) => (
+            <Step>
+              <StepLabel/>
+            </Step>
+          ))}
         </Stepper>
-        
-          <Button onClick={() => nextStep()} variant="outlined">
-            Next Step
-          </Button>
-          <Button onClick={() => previousStep()} variant="outlined">
-            Previous Step
-          </Button>
 
+        <div>
+          {activeStep === steps.length ? (
+            <div>
+              <Typography>All Steps are completed</Typography>
+            </div>
+          ) : (
+            <div>
+              <Typography>{getStepContent(activeStep)}</Typography>
+            </div>
+          )}
+        </div>
+
+        <Button onClick={() => nextStep()} variant="outlined">
+          Next Step
+        </Button>
+        <Button onClick={() => previousStep()} variant="outlined">
+          Previous Step
+        </Button>
       </div>
     );
 }
