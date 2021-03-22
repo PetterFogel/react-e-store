2a0da178418a2eb6d1375e4fbabbdf91bba4421d
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-22 13:37:30 +0100

    btn added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,9 @@ import '../style/Navbar.css'
 import { CartContext } from '../contexts/CartContext'
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import BurgerMenu from './BurgerMenu';
+import { Button } from '@material-ui/core';
+import { btnMedium } from "../style/GeneralStyle";
+import { CSSProperties } from '@material-ui/styles';
 
 interface State {
   isMenuOpen: boolean;
@@ -30,24 +33,31 @@ class Navbar extends Component<{}, State> {
             <h2 className="header-title">ShoeWay</h2>
           </Link>
           <nav>
-            <ul className="nav-links"
+            <ul
+              className="nav-links"
               style={{
-                right: this.state.isMenuOpen ? "0%" : "-100%"
+                right: this.state.isMenuOpen ? "0%" : "-100%",
               }}
             >
-            <Link style={{textDecoration: 'none', color: '#000'}} to="/products">
+              <Link
+                style={{ textDecoration: "none", color: "#000" }}
+                to="/products"
+              >
                 <li>Products</li>
-            </Link>
+              </Link>
+                <li style={adminBtn}>Admin</li>
+              
             </ul>
-              <div className="cart-container">
-                <Link to="/checkout" style={{ color: "#333" }}>
-                    <ShoppingCartOutlinedIcon style={{ fontSize: "1.5rem" }}/>
-                </Link>
-                <div className="cart-content">
-                  {this.context.cart.length}
-                </div>
-          </div>
-            <BurgerMenu value={this.state.isMenuOpen} handleClick={this.handleMenuClick}/>
+            <div className="cart-container">
+              <Link to="/checkout" style={{ color: "#333" }}>
+                <ShoppingCartOutlinedIcon style={{ fontSize: "1.5rem" }} />
+              </Link>
+              <div className="cart-content">{this.context.cart.length}</div>
+            </div>
+            <BurgerMenu
+              value={this.state.isMenuOpen}
+              handleClick={this.handleMenuClick}
+            />
           </nav>
         </header>
       );
@@ -55,3 +65,12 @@ class Navbar extends Component<{}, State> {
 }
 
 export default Navbar
+
+
+const adminBtn: CSSProperties = {
+  margin: "-.5rem 1rem",
+  background: "#56EAC6",
+  padding: ".5rem 2rem",
+  borderRadius: "2rem",
+  color: "#fff",
+};
