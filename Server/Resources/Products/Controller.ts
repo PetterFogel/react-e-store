02d0895901fee653c4e3ac5062ea78424b0bfb11
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-05-18 11:37:22 +0200

    adding new product to database

--- Server/Resources/Products/Controller.ts
+++ Server/Resources/Products/Controller.ts
@@ -1,15 +1,35 @@
 const Product = require('./Model')
 
 module.exports.getProducts = function(req: any, res: any) {
-    res.send(Product);
+    Product.find().sort()
+    .then((result: any) => {
+      res.status(200).json(result)
+      console.log(Product)
+    })
+    .catch((err: any) => {
+      console.log(err);
+    })
 };
 
-module.exports.getSpecific = function (req: any, res: any) {
+module.exports.getSpecific = function(req: any, res: any) {
   res.send("specific");
 };
 
-module.exports.addNewProduct = function (req: any, res: any) {
-  res.send("deleted");
+module.exports.addNewProduct = function(req: any, res: any) {
+  if(req.body){
+    if(!req.body.title){
+      return res.status(400).json('Cant add product')
+    }
+    const product = new Product({
+      title: req.body.title,
+      price: req.body.price,
+      size: req.body.size,
+      quantity: req.body.quantity,
+      category: req.body.category,
+    })
+    product.save()
+    res.status(201).json(product)
+  }
 };
 
 module.exports.deleteProduct = function (req: any, res: any) {
--- Server/Resources/Products/routes.ts
+++ Server/Resources/Products/routes.ts
@@ -7,7 +7,7 @@ const productRouter = express.Router();
 productRouter
     .get('/', controller.getProducts)
     .get('/:id', controller.getSpecific)
-    .post('/', controller.addNewProduct)
+    .post('/addProduct', controller.addNewProduct)
     .delete('/:id', controller.deleteProduct)
     .put('/:id', controller.editProduct)
 
