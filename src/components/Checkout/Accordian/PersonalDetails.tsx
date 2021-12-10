623efc28a07514de6ec100d4de95f8885003b5fd
Petter <petterfogel96@gmail.com>
2021-12-10 10:16:06 +0100

    Implemented the generic textfieldcomponent.

--- src/components/Checkout/Accordian/PersonalDetails.tsx
+++ src/components/Checkout/Accordian/PersonalDetails.tsx
@@ -1,14 +1,11 @@
 import { ChangeEvent, useContext, useState } from "react";
-import {
-  Button,
-  TextField,
-} from "@material-ui/core";
-
+import { Button } from "@material-ui/core";
 import { CSSProperties } from "@material-ui/styles";
-import { UserContext } from '../../../contexts/UserContext'
+import { UserContext } from "../../../contexts/UserContext";
+import TextFieldComponent from "../../UI/TextFieldComponent/TextFieldComponent";
 
 const PersonalDetails = () => {
-  const user = useContext(UserContext)
+  const user = useContext(UserContext);
   const [nameError, setNameError] = useState("");
   const [emailError, setEmailError] = useState("");
   const [adressError, setAdressError] = useState("");
@@ -19,141 +16,100 @@ const PersonalDetails = () => {
     if (!/^[a-öA-Ö\s,'-]+$/.test(e.target.value)) {
       setNameError("Name is not valid");
     } else {
-      setNameError("")
+      setNameError("");
     }
     user.addName(e);
-  }
+  };
 
   const handleAdressChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!/^[a-öA-Ö0-9\s,'-]*$/.test(e.target.value)) {
       setAdressError("Adress is not valid");
     } else {
-      setAdressError("")
+      setAdressError("");
     }
     user.addAdress(e);
-  }
+  };
 
   const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!/^\d{10}$/.test(e.target.value)) {
       setPhoneError("Phone number is not valid");
     } else {
-      setPhoneError("")
+      setPhoneError("");
     }
     user.addPhone(e);
-  }
+  };
 
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(e.target.value)) {
+    if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        e.target.value
+      )
+    ) {
       setEmailError("Email is not valid");
     } else {
-      setEmailError("")
+      setEmailError("");
     }
     user.addEmail(e);
-  }
+  };
 
   const handleZipChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!/^\d{5}$/.test(e.target.value)) {
       setZipError("Zip code is not valid");
     } else {
-      setZipError("")
+      setZipError("");
     }
     user.addZip(e);
-  }
+  };
 
-    return (
-      <form style={form}>
-        <div style={{ marginTop: "1rem" }}>Personal Details</div>
-        <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          required
-          id="name"
-          label="Name"
-          name="name"
-          key="name"
-          type="text"
-          autoComplete="name"
-          autoFocus
-          onChange={handleNameChange}
-          helperText={nameError}
-          error={Boolean(nameError)}
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          required
-          id="adress"
-          key="adress"
-          label="Address"
-          name="address"
-          type="text"
-          autoComplete="address"
-          autoFocus
-          onChange={handleAdressChange}
-          helperText={adressError}
-          error={Boolean(adressError)}
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          required
-          id="phone"
-          type="tel"
-          label="Phone"
-          name="phone"
-          key="phone"
-          autoComplete="phone"
-          autoFocus
-          onChange={handlePhoneChange}
-          helperText={phoneError}
-          error={Boolean(phoneError)}
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          required
-          id="email"
-          type="email"
-          label="Email Adress"
-          name="email"
-          key="email"
-          autoComplete="email"
-          autoFocus
-          onChange={handleEmailChange}
-          helperText={emailError}
-          error={Boolean(emailError)}
-        />
-        <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          required
-          id="zip"
-          label="zip"
-          name="zip"
-          type="number"
-          autoComplete="zip"
-          autoFocus
-          onChange={handleZipChange}
-          helperText={zipError}
-          error={Boolean(zipError)}
-        />
-        <Button
-          type="submit"
-          onClick={user.addToObject}
-          style={btn}
-          variant="contained"
-        >
-          Add
-        </Button>
-      </form>
-    );
-}
-
+  return (
+    <form style={form}>
+      <div style={{ marginTop: "1rem" }}>Personal Details</div>
+      <TextFieldComponent
+        label="Name"
+        type="text"
+        helperText={nameError}
+        error={nameError}
+        onTextFieldChange={handleNameChange}
+      />
+      <TextFieldComponent
+        label="Adress"
+        type="text"
+        helperText={adressError}
+        error={adressError}
+        onTextFieldChange={handleAdressChange}
+      />
+      <TextFieldComponent
+        label="Phone"
+        type="tel"
+        helperText={phoneError}
+        error={phoneError}
+        onTextFieldChange={handlePhoneChange}
+      />
+      <TextFieldComponent
+        label="Email"
+        type="email"
+        helperText={emailError}
+        error={emailError}
+        onTextFieldChange={handleEmailChange}
+      />
+      <TextFieldComponent
+        label="Zip Code"
+        type="number"
+        helperText={zipError}
+        error={zipError}
+        onTextFieldChange={handleZipChange}
+      />
+      <Button
+        type="submit"
+        onClick={user.addToObject}
+        style={btn}
+        variant="contained"
+      >
+        Add
+      </Button>
+    </form>
+  );
+};
 
 const form: CSSProperties = {
   display: "flex",
@@ -180,7 +136,7 @@ const btn: CSSProperties = {
   border: "none",
   cursor: "pointer",
   padding: "1rem",
-  width: '10rem'
+  width: "10rem",
 };
 
-export default PersonalDetails
+export default PersonalDetails;
