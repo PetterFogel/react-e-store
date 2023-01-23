dadf846e7ff9de3cd2b9a901f5770d4f862407b2
PetterFogel <petterfogel96@gmail.com>
2023-01-23 20:14:13 +0100

    Removed redundant component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import { FC } from "react";
-import Layout from "./components/Layout";
+import { FC, CSSProperties } from "react";
+import { Header } from "./common/components/header/Header";
+import MainContent from "./components/Main/MainContent";
+import Background from "./assets/main-bg.jpg";
 
 export const App: FC = () => {
-  return <Layout />;
+  return (
+    <>
+      <Header />
+      <div style={rootStyle}>
+        <MainContent />
+      </div>
+    </>
+  );
+};
+
+const rootStyle: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  backgroundImage: `url(${Background})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  width: "100%",
+  maxHeight: "100vh",
+  overflow: "scroll",
+  overflowX: "hidden",
 };
