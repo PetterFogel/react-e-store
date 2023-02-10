3f6f8b0dee0dc110ad55e856cc954749c5aae517
PetterFogel <petterfogel96@gmail.com>
2023-02-10 15:05:07 +0100

    Implement cart list component

---
+++ src/pages/checkout-page/CartList.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { FC } from "react";
+
+export const CartList: FC = () => {
+  return (
+    <>
+      <Typography variant="h3">Cart List</Typography>
+    </>
+  );
+};
