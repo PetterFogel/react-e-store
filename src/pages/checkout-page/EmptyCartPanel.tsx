3b4d3cd216e8a67f5fa29376e3d0b0366ab7aa80
PetterFogel <petterfogel96@gmail.com>
2023-02-15 08:27:05 +0100

    Implement navigate hook

--- src/pages/checkout-page/EmptyCartPanel.tsx
+++ src/pages/checkout-page/EmptyCartPanel.tsx
@@ -1,13 +1,19 @@
 import { Button, Typography } from "@mui/material";
 import { FC } from "react";
+import { useNavigate } from "react-router";
+import { routeFactory } from "../../common/constants/routeFactory";
 
 export const EmptyCartPanel: FC = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Typography variant="h3" mb={2}>
         Your cart is empty
       </Typography>
-      <Button color="info" variant="contained">
+      <Button
+        color="info"
+        variant="contained"
+        onClick={() => navigate(routeFactory.productScreen.products())}>
         Continue Shopping
       </Button>
     </>
