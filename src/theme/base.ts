dcdbd6e2f8b18e7e675f22da018cb2421eb69bcf
PetterFogel <petterfogel96@gmail.com>
2023-02-15 08:27:18 +0100

    Implement generic color for divider

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -150,7 +150,7 @@ export const theme = createTheme({
       styleOverrides: { root: { cursor: "pointer" } }
     },
     MuiDivider: {
-      styleOverrides: { root: { margin: "15px 0px" } }
+      styleOverrides: { root: { margin: "15px 0px", background: "#ddd" } }
     }
   }
 });
