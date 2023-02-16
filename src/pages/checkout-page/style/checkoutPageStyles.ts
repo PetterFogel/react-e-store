0b609d8215780748065f9408b32dd1bd9bc7f61d
PetterFogel <petterfogel96@gmail.com>
2023-02-16 17:15:29 +0100

    Implement styling for cart item

--- src/pages/checkout-page/style/checkoutPageStyles.ts
+++ src/pages/checkout-page/style/checkoutPageStyles.ts
@@ -3,8 +3,36 @@ import { makeStyles } from "@mui/styles";
 
 export const checkoutPageStyles = makeStyles((theme: Theme) => ({
   root: {
-    maxWidth: "800px",
+    maxWidth: "950px",
     margin: "auto",
-    padding: theme.spacing(2)
+    padding: theme.spacing(2),
+    [theme.breakpoints.down("md")]: {
+      padding: theme.spacing(1.5)
+    }
+  },
+  itemContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  imageHolder: {
+    height: "6rem",
+    flex: 0.7,
+    marginRight: 10
+  },
+  image: {
+    width: "auto",
+    height: "100%"
+  },
+  quantityHolder: {
+    display: "flex",
+    alignItems: "center",
+    gap: "1rem",
+    flex: 1
+  },
+  closeIconHolder: {
+    display: "flex",
+    justifyContent: "space-between",
+    flex: 1
   }
 }));
