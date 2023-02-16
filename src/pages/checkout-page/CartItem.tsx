d71cdff0826f8dd058e3c33f1a7f2c0e57fdadb0
PetterFogel <petterfogel96@gmail.com>
2023-02-16 17:15:15 +0100

    Implement new styling

--- src/pages/checkout-page/CartItem.tsx
+++ src/pages/checkout-page/CartItem.tsx
@@ -5,37 +5,37 @@ import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import CloseIcon from "@mui/icons-material/Close";
 import { CartContext } from "../../contexts/CartContext";
+import { checkoutPageStyles } from "./style/checkoutPageStyles";
 
 interface Props {
   cartProduct: CartProduct;
 }
 
 export const CartItem: FC<Props> = ({ cartProduct }) => {
+  const classes = checkoutPageStyles();
   const { addToCartHandler, quantityChangeHandler, removeFromCartHandler } =
     useContext(CartContext);
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between"
-        }}>
-        <div style={{ height: "6rem" }}>
+      <div className={classes.itemContainer}>
+        <div className={classes.imageHolder}>
           <img
             src={cartProduct.imageUrl}
-            style={{ width: "auto", height: "100%" }}
             alt={cartProduct.imageUrl}
+            className={classes.image}
           />
         </div>
-        <div>
-          <Typography variant={"subtitle1"}>{cartProduct.title}</Typography>
-          <Typography variant={"subtitle1"}>{cartProduct.size}</Typography>
+        <div style={{ flex: 2 }}>
+          <Typography variant={"subtitle1"}>
+            {cartProduct.title} - {cartProduct.size}
+          </Typography>
         </div>
-        <Typography variant={"subtitle1"}>
-          SEK {cartProduct.price}/pcs
-        </Typography>
-        <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+        <div style={{ flex: 1 }}>
+          <Typography variant={"subtitle1"}>
+            SEK {cartProduct.price}/pcs
+          </Typography>
+        </div>
+        <div className={classes.quantityHolder}>
           <RemoveIcon
             fontSize="small"
             onClick={() => quantityChangeHandler(cartProduct.id)}
@@ -46,13 +46,15 @@ export const CartItem: FC<Props> = ({ cartProduct }) => {
             onClick={() => addToCartHandler(cartProduct)}
           />
         </div>
-        <Typography variant={"subtitle1"}>
-          SEK {cartProduct.quantity * cartProduct.price}
-        </Typography>
-        <CloseIcon
-          fontSize="small"
-          onClick={() => removeFromCartHandler(cartProduct)}
-        />
+        <div className={classes.closeIconHolder}>
+          <Typography variant={"subtitle1"}>
+            SEK {cartProduct.quantity * cartProduct.price}
+          </Typography>
+          <CloseIcon
+            fontSize="small"
+            onClick={() => removeFromCartHandler(cartProduct)}
+          />
+        </div>
       </div>
       <Divider />
     </>
