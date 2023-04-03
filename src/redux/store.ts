372aae7365d955010f83222b56877624bbd4a7e0
PetterFogel <petterfogel96@gmail.com>
2023-04-03 15:07:44 +0200

    Implement reusable appthunk type

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
 
 export const store = configureStore({
@@ -7,3 +7,9 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
