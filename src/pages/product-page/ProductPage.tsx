c52f39267e5962a4d7820888048f8efad7602922
PetterFogel <petterfogel96@gmail.com>
2023-04-03 15:11:24 +0200

    Implement logic to fetch products data and render it

--- src/pages/product-page/ProductPage.tsx
+++ src/pages/product-page/ProductPage.tsx
@@ -1,18 +1,21 @@
-import { FC, useContext, useEffect } from "react";
+import { FC, useEffect } from "react";
 import { Loader } from "../../common/components/loader/Loader";
 import { ErrorPanel } from "../../common/components/error-panel/ErrorPanel";
 import { Typography } from "@mui/material";
 import { ProductList } from "./ProductList";
-import { ProductsContext } from "../../contexts/ProductContext";
 import { productPageStyles } from "./style/productPageStyles";
+import { fetchProductsHandler } from "./redux/actions";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { productsSelector } from "./redux/productsSlice";
 
 export const ProductPage: FC = () => {
+  const dispatch = useAppDispatch();
   const classes = productPageStyles();
-  const { products, isProductsLoading, productsError, fetchProductsHandler } =
-    useContext(ProductsContext);
+  const { products, isProductsLoading, productsError } =
+    useAppSelector(productsSelector);
 
   useEffect(() => {
-    fetchProductsHandler();
+    dispatch(fetchProductsHandler());
   }, []);
 
   if (productsError) return <ErrorPanel errorMsg={productsError} />;
