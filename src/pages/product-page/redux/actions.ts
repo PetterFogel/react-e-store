54e95b3e760eb2a55a46926ca71715ebaff8657b
PetterFogel <petterfogel96@gmail.com>
2023-04-03 15:36:21 +0200

    Implement logic to fetch and set product state

--- src/pages/product-page/redux/actions.ts
+++ src/pages/product-page/redux/actions.ts
@@ -3,7 +3,10 @@ import { AppThunk } from "../../../redux/store";
 import {
   setIsProductsLoading,
   setProducts,
-  setProductsError
+  setProductsError,
+  setProduct,
+  setIsProductLoading,
+  setProductError
 } from "./productsSlice";
 
 export const fetchProductsHandler = (): AppThunk => {
@@ -23,6 +26,30 @@ export const fetchProductsHandler = (): AppThunk => {
         const errorMsg = error.message;
         dispatch(setIsProductsLoading(false));
         dispatch(setProductsError(errorMsg));
+      }
+    }
+  };
+};
+
+export const fetchProductHandler = (id: string | undefined): AppThunk => {
+  return async (dispatch) => {
+    try {
+      dispatch(setIsProductLoading(true));
+      dispatch(setProductError(null));
+
+      const response = await axios(
+        `${process.env.REACT_APP_API_BASEURL}/shoes/${id}`
+      );
+
+      console.log(response);
+
+      dispatch(setProduct(response.data));
+      dispatch(setIsProductLoading(false));
+    } catch (error) {
+      if (error instanceof Error) {
+        const errorMsg = error.message;
+        dispatch(setIsProductLoading(false));
+        dispatch(setProductError(errorMsg));
       }
     }
   };
