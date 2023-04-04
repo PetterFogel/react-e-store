779e41d973f6370c62c448a56e702b2387588c13
PetterFogel <petterfogel96@gmail.com>
2023-04-04 15:35:39 +0200

    Implement adminState for rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,11 +1,15 @@
 import { Reducer } from "@reduxjs/toolkit";
 import { ProductState } from "../pages/product-page/redux/types";
 import productsReducer from "../pages/product-page/redux/productsSlice";
+import adminReducer from "../pages/admin-page/redux/adminSlice";
+import { AdminState } from "../pages/admin-page/redux/types";
 
 interface RootReducer {
   productState: Reducer<ProductState>;
+  adminState: Reducer<AdminState>;
 }
 
 export const rootReducer: RootReducer = {
-  productState: productsReducer
+  productState: productsReducer,
+  adminState: adminReducer
 };
