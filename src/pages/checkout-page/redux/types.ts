0fb719684eb3325c8c42169e7e3ceb1ebee99eb8
PetterFogel <petterfogel96@gmail.com>
2023-04-05 14:56:04 +0200

    Implement state for checkout slice

---
+++ src/pages/checkout-page/redux/types.ts
@@ -0,0 +1,12 @@
+import { CartProduct } from "../../../models/cartProduct";
+
+export interface CheckoutState {
+  cartProducts: CartProduct[];
+  totalAmount: number;
+  tax: number;
+  cartOrder: CartProduct[];
+  orderAmount: number;
+  cart: CartProduct[];
+  isCartLoading: boolean;
+  cartError: string | null;
+}
