acef4adf2cbb90d5e0f051e804d749d1bf87cf41
PetterFogel <petterfogel96@gmail.com>
2023-04-11 21:04:42 +0200

    Implement external array with latest content

---
+++ src/pages/home-page/helpers/latestList.ts
@@ -0,0 +1,30 @@
+export const latestList = [
+  {
+    id: 1,
+    title: "New Arrivals",
+    subtitle: "Spring/Summer 2023 essentials.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1535309121587-eea2bc60bf51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  },
+  {
+    id: 2,
+    title: "Comming Soon",
+    subtitle: "Get a preview of upcoming garments.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1535309121587-eea2bc60bf51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  },
+  {
+    id: 3,
+    title: "New Arrivals",
+    subtitle: "Spring/Summer 2023 essentials.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1535309121587-eea2bc60bf51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  },
+  {
+    id: 4,
+    title: "Comming Soon",
+    subtitle: "Get a preview of upcoming garments.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1535309121587-eea2bc60bf51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  }
+];
