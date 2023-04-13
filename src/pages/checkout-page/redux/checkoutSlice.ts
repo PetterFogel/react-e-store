99e51de2004eb511fcdc9edeac000ee6fe7fe8b9
PetterFogel <petterfogel96@gmail.com>
2023-04-13 16:26:20 +0200

    Implement slice for checkout page

---
+++ src/pages/checkout-page/redux/checkoutSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CartProduct } from "../../../models/cartProduct";
+import { RootState } from "../../../redux/store";
+import { CheckoutState } from "./types";
+
+const initialState: CheckoutState = {
+  cart: [],
+  isCartLoading: false,
+  cartError: null,
+  cartProducts: [],
+  cartOrder: [],
+  totalAmount: 0,
+  orderAmount: 0,
+  tax: 0
+};
+
+export const checkoutSlice = createSlice({
+  name: "checkout",
+  initialState,
+  reducers: {
+    setCart: (state, { payload }: PayloadAction<CartProduct[]>) => {
+      state.cart = payload;
+    },
+    setIsCartLoading: (state, { payload }: PayloadAction<boolean>) => {
+      state.isCartLoading = payload;
+    },
+    setCartError: (state, { payload }: PayloadAction<string | null>) => {
+      state.cartError = payload;
+    },
+    setCartProducts: (state, { payload }: PayloadAction<CartProduct[]>) => {
+      state.cartProducts = payload;
+    },
+    setCartOrder: (state, { payload }: PayloadAction<CartProduct[]>) => {
+      state.cartOrder = payload;
+    },
+    setTotalAmount: (state, { payload }: PayloadAction<number>) => {
+      state.totalAmount = payload;
+    },
+    setOrderAmount: (state, { payload }: PayloadAction<number>) => {
+      state.orderAmount = payload;
+    },
+    setTax: (state, { payload }: PayloadAction<number>) => {
+      state.tax = payload;
+    }
+  }
+});
+
+export const {} = checkoutSlice.actions;
+
+export const checkoutSelector = (state: RootState): CheckoutState =>
+  state.checkoutState;
+
+export default checkoutSlice.reducer;
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,15 +1,19 @@
 import { Reducer } from "@reduxjs/toolkit";
 import { ProductState } from "../pages/product-page/redux/types";
+import { AdminState } from "../pages/admin-page/redux/types";
+import { CheckoutState } from "../pages/checkout-page/redux/types";
 import productsReducer from "../pages/product-page/redux/productsSlice";
 import adminReducer from "../pages/admin-page/redux/adminSlice";
-import { AdminState } from "../pages/admin-page/redux/types";
+import checkoutReducer from "../pages/checkout-page/redux/checkoutSlice";
 
 interface RootReducer {
   productState: Reducer<ProductState>;
   adminState: Reducer<AdminState>;
+  checkoutState: Reducer<CheckoutState>;
 }
 
 export const rootReducer: RootReducer = {
   productState: productsReducer,
-  adminState: adminReducer
+  adminState: adminReducer,
+  checkoutState: checkoutReducer
 };
