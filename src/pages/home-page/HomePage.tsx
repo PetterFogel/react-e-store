753d1a5b0054369b68ef7c4995b51c4cf725aa46
PetterFogel <petterfogel96@gmail.com>
2023-05-09 17:58:58 +0200

    Implement keyframes animation for introduction

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,36 +1,22 @@
 import { FC } from "react";
-import { useNavigate } from "react-router";
-import { homePageStyles } from "./style/homePageStyles";
-import { Button, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Typography } from "@mui/material";
 import { latestList } from "./helpers/latestList";
 import { SectionList } from "../../common/components/section-list/SectionList";
+import { homePageStyles } from "./style/homePageStyles";
 
 export const HomePage: FC = () => {
   const classes = homePageStyles();
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const isBreakpointMd = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
-    <div>
-      <section className={classes.introContainer}>
-        <div className={classes.introHolder}>
-          <Typography variant="h1">
-            WELCOME TO <br /> THE MANY SAINTS
-          </Typography>
-          <Typography variant="h2" mt={2} mb={2}>
-            Quality clothes to match your style
-          </Typography>
-          <Button
-            onClick={() => navigate("/products")}
-            color="info"
-            variant="contained"
-            size={isBreakpointMd ? "medium" : "large"}>
-            Go Shopping
-          </Button>
-        </div>
-      </section>
+    <section>
+      <div className={classes.heroSection}></div>
+      <Typography variant="h1">
+        Welcome to the The Many Saints, quality clothes to match your style
+      </Typography>
       <SectionList sectionList={latestList} />
-    </div>
+      <div className={classes.heroSection2}>
+        <div className={classes.diamond} />
+      </div>
+    </section>
   );
 };
--- src/pages/home-page/style/HomePageStyles.ts
+++ src/pages/home-page/style/HomePageStyles.ts
@@ -17,5 +17,54 @@ export const homePageStyles = makeStyles((theme: Theme) => ({
     [theme.breakpoints.down("md")]: {
       padding: theme.spacing(1)
     }
+  },
+  heroSection: {
+    position: "relative",
+    height: "94vh",
+    width: "100%",
+    overflow: "hidden",
+    background:
+      "linear-gradient(90deg, #f12711, #f5af19, #3dc5b3, #41b3e6, #7b53e6, #f12711)",
+    backgroundSize: "1200% 1200%",
+    animation: "$changeGradient 30s ease-in-out infinite"
+  },
+  "@keyframes changeGradient": {
+    "0%": {
+      backgroundPosition: "0% 50%"
+    },
+    "50%": {
+      backgroundPosition: "100% 50%"
+    },
+    "100%": {
+      backgroundPosition: "0% 50%"
+    }
+  },
+  heroSection2: {
+    position: "relative",
+    height: "calc(100vh - 80px)",
+    width: "100%",
+    overflow: "hidden"
+  },
+  diamond: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "50px",
+    height: "50px",
+    background: "#f5af19",
+    clipPath: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
+    animation: "$moveDiamond 10s linear infinite"
+  },
+  "@keyframes moveDiamond": {
+    "0%": {
+      transform: "translate(-50%, -50%) rotate(0deg)"
+    },
+    "50%": {
+      transform: "translate(-50%, -50%) rotate(180deg)"
+    },
+    "100%": {
+      transform: "translate(-50%, -50%) rotate(360deg)"
+    }
   }
 }));
